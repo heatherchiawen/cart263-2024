76bce377dfc068fe414c77d28d1e990d64ff68c8
Heather Chester <h_cheste@live.concordia.ca>
2024-02-04 22:26:39 -0500

    Added basic info and code to start working on the program

--- exercises/voice-jam/js/script.js
+++ exercises/voice-jam/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Voice Jam 
+ * Heather Chester 
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -15,11 +15,17 @@ function preload() {
 
 }
 
+let voice = new p5.Speech(); 
+let speechRecognizer = new p5.SpeechRec(); 
 
 /**
  * Description of setup
 */
 function setup() {
+    createCanvas(windowHeight, windowWidth); 
+    //speechRecognizer.onResult = handle Result; // Set up function to use 
+    //speechRecogniser.continuous = true; 
+    //speechRecognizer.start(); 
 
 }
 
@@ -28,5 +34,6 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(100, 0, 50); 
 
 }
