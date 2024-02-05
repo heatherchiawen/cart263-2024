b245aadb693d6ca169b0575fd59fd2f07684bf12
Heather Chester <h_cheste@live.concordia.ca>
2024-02-04 21:24:02 -0500

    Switched to p5 speech link in HTML to CDN and added code from instructions

--- activities/slamina/js/script.js
+++ activities/slamina/js/script.js
@@ -1,9 +1,7 @@
 /**
- * Title of Project
- * Author Name
+ * Slamina Activity
+ * Heather Chester
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
  */
 
 "use strict";
@@ -12,21 +10,186 @@
  * Description of preload
 */
 function preload() {
-
 }
 
+const animals = [
+    "aardvark",
+    "alligator",
+    "alpaca",
+    "antelope",
+    "ape",
+    "armadillo",
+    "baboon",
+    "badger",
+    "bat",
+    "bear",
+    "beaver",
+    "bison",
+    "boar",
+    "buffalo",
+    "bull",
+    "camel",
+    "canary",
+    "capybara",
+    "cat",
+    "chameleon",
+    "cheetah",
+    "chimpanzee",
+    "chinchilla",
+    "chipmunk",
+    "cougar",
+    "cow",
+    "coyote",
+    "crocodile",
+    "crow",
+    "deer",
+    "dingo",
+    "dog",
+    "donkey",
+    "dromedary",
+    "elephant",
+    "elk",
+    "ewe",
+    "ferret",
+    "finch",
+    "fish",
+    "fox",
+    "frog",
+    "gazelle",
+    "gila monster",
+    "giraffe",
+    "gnu",
+    "goat",
+    "gopher",
+    "gorilla",
+    "grizzly bear",
+    "ground hog",
+    "guinea pig",
+    "hamster",
+    "hedgehog",
+    "hippopotamus",
+    "hog",
+    "horse",
+    "hyena",
+    "ibex",
+    "iguana",
+    "impala",
+    "jackal",
+    "jaguar",
+    "kangaroo",
+    "koala",
+    "lamb",
+    "lemur",
+    "leopard",
+    "lion",
+    "lizard",
+    "llama",
+    "lynx",
+    "mandrill",
+    "marmoset",
+    "mink",
+    "mole",
+    "mongoose",
+    "monkey",
+    "moose",
+    "mountain goat",
+    "mouse",
+    "mule",
+    "muskrat",
+    "mustang",
+    "mynah bird",
+    "newt",
+    "ocelot",
+    "opossum",
+    "orangutan",
+    "oryx",
+    "otter",
+    "ox",
+    "panda",
+    "panther",
+    "parakeet",
+    "parrot",
+    "pig",
+    "platypus",
+    "polar bear",
+    "porcupine",
+    "porpoise",
+    "prairie dog",
+    "puma",
+    "rabbit",
+    "raccoon",
+    "ram",
+    "rat",
+    "reindeer",
+    "reptile",
+    "rhinoceros",
+    "salamander",
+    "seal",
+    "sheep",
+    "shrew",
+    "silver fox",
+    "skunk",
+    "sloth",
+    "snake",
+    "squirrel",
+    "tapir",
+    "tiger",
+    "toad",
+    "turtle",
+    "walrus",
+    "warthog",
+    "weasel",
+    "whale",
+    "wildcat",
+    "wolf",
+    "wolverine",
+    "wombat",
+    "woodchuck",
+    "yak",
+    "zebra"
+]; 
+  
+let currentAnimal = ``;
+
+const voice = new p5.Speech(); // For speech output 
+// let speechRecognizer = new p5.SpeechRec(); 
 
 /**
  * Description of setup
 */
 function setup() {
-
+    createCanvas(windowWidth, windowHeight); 
 }
-
 
 /**
  * Description of draw()
 */
 function draw() {
 
+}
+
+function mousePressed() {
+    // Assign random animal name from animal name array to currentAnimal 
+    currentAnimal = random(animals); 
+    // Declare reverseAnimal and assign the reverse of currentAnimal to it by using reverseString(currentAnimal);
+    let reverseAnimal = reverseString(currentAnimal);  
+    // Use speechSynthesizer to speack reverse Animal 
+    voice.speak(reverseAnimal); 
+}
+
+/**
+Reverses the provided string
+*/
+function reverseString(string) {
+    // Split the string into an array of characters
+    let characters = string.split(``);
+    // Reverse the array of characters
+    let reverseCharacters = characters.reverse();
+    // Join the array of characters back into a string
+    let result = reverseCharacters.join(``);
+    // Return the result
+    return result;
+
+// You could also combine all this into:
+// return string.split(``).reverse().join(``);
 }
