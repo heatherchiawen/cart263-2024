fdc4d974cb5a4d880b828a01f63fbdfb370412c5
Heather Chester <h_cheste@live.concordia.ca>
2024-02-05 20:35:24 -0500

    Attempting to have the questions and heard answers to be said by the program in sequential order

--- exercises/voice-jam/js/script.js
+++ exercises/voice-jam/js/script.js
@@ -18,7 +18,6 @@ function preload() {
 let voice = new p5.Speech(); // Speech synthesizer 
 let speechRecognizer = new p5.SpeechRec(); // Speech Recognizer 
 
-let currentChatSpeech = ``; // Current speech for the bot 
 let currentSpeech = ``; // What is currently being said 
 
 let endSpeech = `Looks like we have found your ideal match! Please click the button below to reveal`; 
@@ -26,31 +25,26 @@ let endSpeech = `Looks like we have found your ideal match! Please click the but
 let data = [ {
     category: `Relationship`,
     question: `What kind of relationship are you looking for?`,
-    answer: ` `, 
     heard: `You said: Taxing`
 }, 
 {
     category: `Hobbies`,
     question: `What are some of your hobbies?`,
-    answer: ` `, 
     heard: `You said: Increasing tuition fees for out-of-province and international students and ensuring public sector workers are not paid a livable wage all while making sure there is a surplus of money in my pocket`
 }, 
 {
     category: `Traits`,
     question: `What traits do you seek for in your ideal partner?`,
-    answer: ` `, 
     heard: `You said: Deep pockets with a passion for aggravating the masses`
 }, 
 {
     category: `Age`,
     question: `Age is just a number... but what number are you looking for?`,
-    answer: ` `, 
     heard: `You said: 66...6`
 }, 
 {
     category: `Looks`, 
     question: `Describe your dream appearance for your potential partner`,
-    answer: ` `, 
     heard: `You said: Privileged cis-het white man who should retire`,
 }
 ];
@@ -65,14 +59,9 @@ let showSubtitle = false;
 */
 function setup() {
     createCanvas(windowHeight, windowWidth); 
-    speechRecognizer.onResult = handleResult; // Set up function to use 
+    speechRecognizer.onResult = handleResult; 
     speechRecognizer.start(); 
     speechRecognizer.continuous = true; 
-
-    // if (speechRecognizer) {
-    //     speechRecognizer.start(); 
-    //     speechRecognizer.addCallback(`result`, handleResult); 
-    // }
 
     // // To call text when speaking 
     // voice.onStart = speechStarted; 
@@ -86,28 +75,40 @@ function setup() {
 function draw() {
     background(230, 181, 223); 
 
+    if (speechRecognizer.resultString) {
+        handleResult(currentQuestion); 
+    }
+
     displayQuestion(); 
-    displayProfile();
+    // displayProfile();
 }
 
 function mousePressed() {
     voice.speak(data[currentQuestion].question); 
+    
     // Add something that waits for the user to answer 
 }
 
 function handleResult() { 
-   // If program detects sound  
-   if (speechRecognizer.resultValue) {
-    // Then repeat what is "heard"
-    data[currentQuestion].answer = data[currentQuestion.heard]; 
-    // Then move on to the next question 
-   }
-    // Go to the next question 
-    currentQuestion++; 
-    // If last question then stop listening // this sound be an else if statement ???
-    if (currentQuestion >= data.length) {
-        voice.removeCallback(`result`); 
+
+    // To see if it is detecting speech input 
+    if (speechRecognizer.resultValue === true) {
+        console.log(speechRecognizer.resultString); 
     }
+
+    // currentSpeech = speechRecognizer.resultString; 
+    // If program detects sound  
+    if (speechRecognizer.resultValue) {
+        // Then repeat what is "heard"
+        voice.speak(data[currentQuestion].heard); 
+        // Go to the next question 
+        currentQuestion++;
+    }
+    
+    // // If last question then stop listening // this sound be an else if statement ???
+    // if (currentQuestion >= data.length) {
+    //     voice.removeCallback(`result`); 
+    // }
 }
 
 function displayQuestion() { // Copied from misheard-dating-profile code by Pippin 
@@ -141,3 +142,5 @@ function displayProfile() { // Copied from misheard-dating-profile code by Pippi
     text(dataString, 100, 200);
     pop();
   }
+
+  
