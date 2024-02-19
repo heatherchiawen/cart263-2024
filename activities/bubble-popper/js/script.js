cd3471e0dc4229210f256b60f63366002b56bf2a
Heather Chester <h_cheste@live.concordia.ca>
2024-02-19 16:16:00 -0500

    Added handpose functions and bubbles

--- activities/bubble-popper/js/script.js
+++ activities/bubble-popper/js/script.js
@@ -16,6 +16,8 @@ let video = undefined;
 let handpose = undefined; 
 // Current set of predictions 
 let predictions = []; 
+// Bubble 
+let bubble = undefined; 
 
 function preload() {
 
@@ -44,6 +46,15 @@ function setup() {
         console.log(results); 
         predictions = results; 
     }); 
+
+    // 
+    bubble = {
+        x: random(width),
+        y: height,
+        size: 100, 
+        vx: 0, 
+        vy: -2
+    }
 }
 
 
@@ -52,5 +63,52 @@ function setup() {
 */
 function draw() {
     background(0); 
+
+    // Handpose 
+    if (predictions.length > 0) {
+        let hand = predictions[0]; 
+        let index = hand.annotations.indexFinger; 
+        let tip = index[3]; 
+        let base = index[0]; 
+        let tipX = tip[0]; 
+        let tipY = tip[1]; 
+        let baseX = base[0]; 
+        let baseY = base[1]; 
+
+        push();
+        noFill(); 
+        stroke(255); 
+        strokeWeight(5); 
+        line(baseX, baseY, tipX, tipY); 
+        pop(); 
+
+        push(); 
+        noStroke(); 
+        fill(255, 0, 0); 
+        ellipse(baseX, baseY, 20); 
+        pop(); 
+
+        // Check bubble popping 
+        let d = dist(tipX, tipY, bubble.x, bubble.y); 
+        if  (d< bubble.size/2) {
+            bubble.x = random(width); 
+            bubble.y = height; 
+        }
+    }
+
+    // Move the bubble 
+    bubble.x += bubble.vx; 
+    bubble.y += bubble.vy; 
+
+    if (bubble.y < 0) {
+        bubble.x = random(width); 
+        bubble.y = height; 
+    }
+
+    push();
+    fill(0, 100, 200); 
+    noStroke(); 
+    ellipse(bubble.x, bubble.y, bubble.size); 
+    pop(); 
 
 }
