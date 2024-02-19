691dd42527562307367f6d32e878e575b3ab3469
Heather Chester <h_cheste@live.concordia.ca>
2024-02-19 16:25:06 -0500

    Added new file to exercise folder for AI-jam assignment and added the basic set up

---
+++ exercises/AI-jam/js/script.js
@@ -0,0 +1,54 @@
+/**
+ * Title of Project
+ * Heather Chester 
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+
+let video = undefined; 
+let handpose = undefined; 
+let predictions = []; 
+
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+    createCanvas(640, 480); 
+
+    video = createCapture(VIDEO); 
+    video.hide(); 
+
+    handpose = ml5.handpose(video, {
+        fliphorizontal: true 
+    }, function() {
+        console.log(`Model loaded.`); 
+    }); 
+
+    handpose.on(`predict`, function(results) {
+        console.log(results); 
+        predictions = results;  
+    });
+
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+    background(0); 
+
+}
