14c6bf452b4b2236530b52d1c5262d11da020960
Heather Chester <h_cheste@live.concordia.ca>
2024-03-14 17:15:48 -0400

    Add the rest of the notes from the sprites tutorial

--- experiments/Game-Engine/js/Boot.js
+++ experiments/Game-Engine/js/Boot.js
@@ -8,7 +8,21 @@ class Boot extends Phaser.Scene {
     // preload() method tells Phaser 3 that we want to preload asset files here 
     preload() {
         // EXAMPLE: https://phaser.io/examples/v3/view/loader/image/load-image
-        this.load.image(`wall`, `assets/images/wall.png`); 
+        this.load.image(`wall`, `assets/images/wall.png`);
+        
+        // How to load a simple spirte sheet using the loader's.spritesheet() method 
+        // The parameters are (key, image file, comfiguration of object specfifying the 
+        // dimentions of an individual frame via frameWidth and frameHeight, the final frame number (counting from 0))
+        // EXAMPLE: https://phaser.io/examples/v3/view/loader/sprite-sheet/load-sprite-sheet
+        this.load.spritesheet(`avatar`, `assets/images/avatar.png`, {
+            // Animation has 32x32 pixel frames 
+            frameWidth: 32, 
+            frameHeight: 32, 
+            // Our animation has 4 frames, so the final frame number is 3, counting from 0
+            endFrame: 3
+        }); 
+       
+       
         // `complete` acts as an event listener to tell the porgram to switch scenes once the asset is loaded
         // EXAMPLE: https://phaser.io/examples/v3/view/loader/loader-events/load-progress
         this.load.on(`complete`, () => {
--- experiments/Game-Engine/js/Play.js
+++ experiments/Game-Engine/js/Play.js
@@ -35,6 +35,51 @@ class Play extends Phaser.Scene {
         // HEXADEMICAL colour notation, but written with a 0x in front of the colour code instead of a #
         // EXAMPLE: https://phaser.io/examples/v3/view/display/tint/single-color-tint
         this.wall.setTint(0xdd3333); 
+
+        // Adding the avatar in the same way as Boot, but providing the key of the sprite sheet
+        this.avatar = this.add.sprite(200, 200, `avatar`); 
+        
+        // Avatar starts out as "idle"
+        this.avatar.play(`idle`); 
+    }
+
+    createAnimation() {
+        // Here we create an animation we will play when the avatar is moving in game 
+        // Created using the animation system of Phaser 3, via this.anims and used in .create()
+        // EXAMPLE: https://phaser.io/examples/v3/view/loader/sprite-sheet/load-sprite-sheet
+        let movingAnimationConfig = { 
+            // Animation key nused when playing the sprite animation 
+            key: `moving`, 
+            frames: this.anims.generateFrameNumbers(`avatar`, {
+                // Plays from first frame 
+                start: 0, 
+                // Last frame 
+                end: 3
+            }), 
+            // The frame rate this animation should play at 
+            frameRate: 30,
+            // Hoe many times to repeat the animation 
+            // 0 means not to play at all 
+            // Any positive number (e.g. 3) means to play that many loops (e.g. three times)
+            // -1 means to loop infinitely 
+            repeat: -1
+        }
+
+        this.anims.create(movingAnimationConfig); 
+
+        // Configuration an idle animation 
+        let ideleAnimationConfig = {
+            // Different animation key 
+            key: `idle`, 
+            frames: this.anims.generateFrameNumbers(`avatar`, {
+                // Only using 0, so the frames styart and end there 
+                start: 0, 
+                end: 0
+            }), 
+            // No need to specify a frame rate for somthing that doesn't animate 
+            repeat: 0
+        };
+        this.anims.create(idleAnimationConfig); 
     }
     // update() is basically the draw 
     update() {
