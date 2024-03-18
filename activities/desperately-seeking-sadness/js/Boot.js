275f978568738e7fade11f53196b26c08c6df13b
Heather Chester <h_cheste@live.concordia.ca>
2024-03-18 17:00:20 -0400

    Added thumbs-up and notes

--- activities/desperately-seeking-sadness/js/Boot.js
+++ activities/desperately-seeking-sadness/js/Boot.js
@@ -8,8 +8,10 @@ class Boot extends Phaser.Scene {
     preload() {
         // Load assets here 
         this.load.image(`avatar`, `assets/images/avatar.png`); 
-        this.load.image(`thumbs-down`, `assets/images/thumbs-down.png`); 
+        this.load.image(`thumbs-down`, `assets/images/thumbs-down.png`);
+        this.load.image(`thumbs-up`, `assets/images/thumbs-up.png`);  
 
+        // After Boot is loaded in main, this triggers play to start 
         this.load.on(`complete`, () => {
             this.scene.start(`play`); 
         });
--- activities/desperately-seeking-sadness/js/Play.js
+++ activities/desperately-seeking-sadness/js/Play.js
@@ -10,14 +10,44 @@ class Play extends Phaser.Scene {
         this.avatar = this.physics.add.sprite(400, 300, `avatar`); 
         this.avatar.setCollideWorldBounds(true); 
 
+        // For thumbs-down 
         let x = Math.random() * this.sys.canvas.width; 
         let y = Math.random() * this.sys.canvas.height; 
         this.sadness = this.physics.add.sprite(x, y, `thumbs-down`); 
 
+        // Thumbs-up group
+        this.happiness = this.physics.add.group({
+            key: `thumbs-up`, 
+            quantity: 120, 
+            bounceX: 0.5, 
+            bounceY: 0.5, 
+            colliedrWorldBounds: true, 
+            dragX: 50, 
+            dragY: 50
+        }); 
+        Phaser.Actions.RandomRectangle(this.happiness.getChildren(), this.physics.world.bounds); 
+
+        // Checks for overlap/collide with avatar and sadness/ avatar and happiness 
+        this.physics.add.overlap(this.avatar, this.sadness, this.getSad, null, this); 
+        this.physics.add.collider(this.avatar, this.happiness); 
+        this.physics.add.collider(this.happiness, this.happiness); 
+
+        // Thumbs-down over-lap check with getSad()
+        this.physics.add.overlap(this.avatar, this.sadness, this.getSad, null, this); 
+
+        // Activates the users keys in update()
         this.cursors = this.input.keyboard.createCursorKeys(); 
     }
 
+    getSad(avatar, sadness) {
+        // Thumbs-down appears at random position 
+        let x = Math.random() * this.sys.canvas.width; 
+        let y = Math.random() * this.sys.canvas.height; 
+        this.sadness.setPosition(x, y); 
+    }
+
     update() {
+        // Conditionals for the users keyboard to control the velocity of the avatar 
         if (this.cursors.left.isDown) {
             this.avatar.setAngularVelocity(-150); 
         }
