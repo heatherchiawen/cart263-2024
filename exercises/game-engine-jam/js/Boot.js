7936ca98bf9cf3fe80d53fff9f1d5bfa51b9156a
Heather Chester <h_cheste@live.concordia.ca>
2024-03-18 19:20:47 -0400

    Animated drumstick sprite

--- exercises/game-engine-jam/js/Boot.js
+++ exercises/game-engine-jam/js/Boot.js
@@ -7,6 +7,14 @@ class Boot extends Phaser.Scene {
 
     preload() {
         // Load assets here 
+        // this.load.image(`avatar`, `assets/images/avatar.png`); 
+
+        //Sprite sheet 
+        this.load.spritesheet(`avatar`, `assets/images/avatar.png`, {
+            frameWidth: 32, 
+            frameHeight: 32, 
+            endFrame:3
+        }); 
 
         // After Boot loads, triggers play 
         this.load.on(`complete`, () => {
--- exercises/game-engine-jam/js/Play.js
+++ exercises/game-engine-jam/js/Play.js
@@ -6,10 +6,25 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+        this.avatar = this.add.sprite(200, 200, `avatar`); 
+        // this.avatar.setColliderWorldBounds(true); 
+
+        // Moving the avatar 
+        let movingAnimationConfig = {
+            key: `moving`, 
+            frames: this.anims.generateFrameNumbers(`avatar`, {
+                start: 0, 
+                end: 3
+            }), 
+            frameRate: 10, 
+            repeat: -1 // Positive numbers play that many loops, -1 plays the loop indefinitely 
+        }; 
+        this.anims.create(movingAnimationConfig); 
+        this.avatar.play(`moving`); 
 
     }
 
     update() {
-        
+
     }
 }
