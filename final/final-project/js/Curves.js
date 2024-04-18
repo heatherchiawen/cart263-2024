95b395265a316cdb57d852d53a67e50f828558a2
Heather Chester <h_cheste@live.concordia.ca>
2024-04-17 23:07:05 -0400

    last attempt at solving within class, might try to solve with code from proposal

--- final/final-project/js/Curves.js
+++ final/final-project/js/Curves.js
@@ -13,75 +13,59 @@ class Curves {
         this.e = e; 
         this.f = f; 
 
-        this.vx = random(-1, 1); 
-        this.vy = random(-1, 1); 
-        this.va = random(-1, 1); 
-        this.vb = random(-1, 1); 
-        this.vc = random(-1, 1); 
-        this.vd = random(-1, 1); 
-        this.ve = random(-1, 1); 
-        this.vf = random(-1, 1); 
+        // this.vx = random(-1, 1); 
+        // this.vy = random(-1, 1); 
+        // this.va = random(-1, 1); 
+        // this.vb = random(-1, 1); 
+        // this.vc = random(-1, 1); 
+        // this.vd = random(-1, 1); 
+        // this.ve = random(-1, 1); 
+        // this.vf = random(-1, 1); 
+
+        this.vx = 0; 
+        this.vy = 0; 
+        this.va = 0; 
+        this.vb = 0; 
+        this.vc = 0; 
+        this.vd = 0; 
+        this.ve = 0; 
+        this.vf = 0; 
+        this.speed = 1; 
     }
 
     move() {
-        if (this.x < 0 || this.y < 0 || this.a < 0 || this.b < 0 || this.c < 0 || this.d < 0 || this.e < 0 || this.f < 0 || 
-            this.x > width || this.y > width || this.a > width || this.b > width || this.c > width || this.d > width || this.e > width || this.f > width || 
-            this.x > height || this.y > height || this.a > height || this.b > height || this.c > height || this.d > height || this.e > height || this.f > height) {
-        // this.x -= this.vx;
-        // this.y -= this.vy;  
-        // this.a -= this.va;
-        // this.b -= this.vb;
-        // this.c -= this.vc;
-        // this.d -= this.vd;
-        // this.e -= this.ve;
-        // this.f -= this.vf; 
-        this.vx -= this.vx;
-        this.vy -= this.vy;  
-        this.va -= this.va;
-        this.vb -= this.vb;
-        this.vc -= this.vc;
-        this.vd -= this.vd;
-        this.ve -= this.ve;
-        this.vf -= this.vf; 
+        if (this.x < 0 && this.y < 0 && this.a < 0 && this.b < 0 && this.c < 0 && this.d < 0 && this.e < 0 && this.f < 0) {
+            this.x += this.speed; 
+            this.y += this.speed; 
+            this.a += this.speed; 
+            this.b += this.speed; 
+            this.c += this.speed; 
+            this.d += this.speed; 
+            this.e += this.speed; 
+            this.f += this.speed; 
 
-        } else {
-        this.x += this.vx; 
-        this.y += this.vy; 
-        this.a += this.va; 
-        this.b += this.vb; 
-        this.c += this.vc; 
-        this.d += this.vd; 
-        this.e += this.ve; 
-        this.f += this.vf; 
+        } if (this.x > width && this.y > width && this.a > width && this.b > width && this.c > width && this.d > width && this.e > width && this.f > width &&
+            this.x > height && this.y > height && this.a > height && this.b > height && this.c > height && this.d > height && this.e > height && this.f > height) {
+                this.x -= this.speed; 
+                this.y -= this.speed; 
+                this.a -= this.speed; 
+                this.b -= this.speed; 
+                this.c -= this.speed; 
+                this.d -= this.speed; 
+                this.e -= this.speed; 
+                this.f -= this.speed; 
         }
-
-        // this.vx 
-        // this.vy  
-        // this.va 
-        // this.vb 
-        // this.vc 
-        // this.vd 
-        // this.ve 
-        // this.vf 
-
-        // this.x
-        // this.y 
-        // this.a
-        // this.b
-        // this.c
-        // this.d 
-        // this.e 
-        // this.f 
-
-        // I dont think this is actually doing anything 
-        // this.x = constrain(this.x, 0, width); 
-        // this.y = constrain(this.y, 0, height); 
-        // this.a = constrain(this.a, 0, height); 
-        // this.b = constrain(this.b, 0, height); 
-        // this.c = constrain(this.c, 0, height); 
-        // this.d = constrain(this.d, 0, height); 
-        // this.e = constrain(this.e, 0, height); 
-        // this.f = constrain(this.f, 0, height); 
+        
+        // else {
+        //     this.x += this.vx; 
+        //     this.y += this.vy; 
+        //     this.a += this.va; 
+        //     this.b += this.vb; 
+        //     this.c += this.vc; 
+        //     this.d += this.vd; 
+        //     this.e += this.ve; 
+        //     this.f += this.vf; 
+        // }
     } 
 
     display() {
